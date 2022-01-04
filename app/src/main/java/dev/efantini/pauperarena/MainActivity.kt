c6e6503fbdea29f04374079288a671f0eee45abf
PizzaMarinara <pizzamarinara@protonmail.com>
2022-01-04 12:17:55 +0100

    More theming and splash screen

--- app/src/main/java/dev/efantini/pauperarena/MainActivity.kt
+++ app/src/main/java/dev/efantini/pauperarena/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.efantini.pauperarena
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.efantini.pauperarena.ui.elements.MainContent
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MainContent()
         }
