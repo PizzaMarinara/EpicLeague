90ba81bea9f535f2c2ab2ccc4c00edc5534cbab3
PizzaMarinara <pizzamarinara@protonmail.com>
2022-01-26 21:38:37 +0100

    Updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-    val gradlePluginVersion by extra { "7.0.4" }
+    val gradlePluginVersion by extra { "7.1.0" }
     val hiltVersion by extra { "2.38.1" }
     val kotlinVersion by extra { "1.6.10" }
     val objectboxVersion by extra { "3.1.0" }
